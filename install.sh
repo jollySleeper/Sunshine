0e62c3e38e03f20c3c99b2fe1bfde4928c74b5d6
jollySleeper <git@pm21.anonaddy.com>
2023-05-25 16:58:41 +0530

    Sunshine Notifies with an Icon which get Automatically Installed

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+install_icon () {
+    if [[ ! -d $HOME/.local/share/icons ]]; then
+        mkdir -p $HOME/.local/share/icons  
+    fi
+    cp sun-with-face.svg $HOME/.local/share/icons/.
+}
+
+# Application
+# TODO: Check if Already Installed
 if [[ ! -d $HOME/.local/bin ]]; then
    mkdir -p $HOME/.local/bin/  
 fi
 
 cp sunshine $HOME/.local/bin/.
+install_icon
 cd $HOME/.local/bin
 chmod 774 sunshine
 
